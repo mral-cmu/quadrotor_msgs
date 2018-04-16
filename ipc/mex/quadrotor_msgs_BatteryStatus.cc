92b5ebe57fd80e53125bdd31b036e0e9fca11649
Mosam Dabhi <mosam.dabhi@gmail.com>
2018-04-16 15:48:10 -0400

    Correcting feature/explicit_epc branch

---
+++ ipc/mex/quadrotor_msgs_BatteryStatus.cc
@@ -0,0 +1,6 @@
+#include <ipc_bridge/msgs/quadrotor_msgs_BatteryStatus.h>
+#include <ipc_bridge/matlab/quadrotor_msgs_BatteryStatus.h>
+
+#define MESSAGE_NAME quadrotor_msgs::BatteryStatus
+
+#include <ipc_bridge/mex_interface.h>
